7c07a661c2d8908ddb9754d15352a79babe4f764
Ruhan Gonçalves <ruhan.goncalves@gmail.com>
2020-01-01 20:36:36 -0300

    Adiciona escrita e leitura de arquivos a jogo da forca

--- parte_2/jogos/forca.py
+++ parte_2/jogos/forca.py
@@ -1,8 +1,22 @@
+import random
+
 def jogar():
     print("JOGO DA FORCA")
-    palavra_secreta="banana"
-    palavra_secreta=palavra_secreta.lower()
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].lower()
     letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou=False
     tentativas=0
