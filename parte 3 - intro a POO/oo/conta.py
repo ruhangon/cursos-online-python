122842065dd6ceab3628cf9ab975fdb70fd665b3
Ruhan Gonçalves <ruhan.goncalves@gmail.com>
2020-05-17 14:14:45 -0300

    Estuda encapsulamento de uma classe

--- parte 3 - intro a POO/oo/conta.py
+++ parte 3 - intro a POO/oo/conta.py
@@ -2,21 +2,25 @@ class Conta:
     # função abaixo é executada na criação de um objeto, construtor
     def __init__(self, numero, titular, saldo, limite):
         # se criar método passando limite=1000.0 esse será o valor padrão. Então pode ser criado o objeto sem essa informação, que ele já terá o valor padrão
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo+=valor
+        self.__saldo+=valor
         print("valor adicionado a conta")
 
     def saca(self, valor):
-        self.saldo-=valor
+        self.__saldo-=valor
         print("valor retirado da conta")
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
 
     # a palavra chave pass serve para fazer esse código funcionar
     pass
--- parte 3 - intro a POO/oo/teste_das_contas.py
+++ parte 3 - intro a POO/oo/teste_das_contas.py
@@ -1,10 +1,11 @@
 from conta import Conta
 
-conta = Conta(10, "Jorge", 100.0, 1000.0)
+conta1 = Conta(10, "Jorge", 100.0, 1000.0)
+conta1.extrato()
+conta2 = Conta(20, "Mário", 200.0, 1500.0)
+conta2.extrato()
 
-conta.deposita(150)
-conta.extrato()
-
-conta.saca(100)
-conta.extrato()
+conta1.transfere(20, conta2)
+conta1.extrato()
+conta2.extrato()
 
