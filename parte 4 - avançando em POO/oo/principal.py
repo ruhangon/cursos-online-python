2cbd965243f49355ccfa90b3a85bef529bc3bf1b
Ruhan Gonçalves <ruhan.goncalves@gmail.com>
2020-06-29 11:19:34 -0300

    Continua estudo de herança

--- parte 4 - avançando em POO/oo/principal.py
+++ parte 4 - avançando em POO/oo/principal.py
@@ -1,17 +1,30 @@
 from filme import Filme
 from serie import Serie
+from playlist import Playlist
 
-vingadores=Filme('vingadores, guerra infinita', 2018, 160)
+vingadores_g_i=Filme('vingadores, guerra infinita', 2018, 160)
+vingadores_u=Filme('vingadores, ultimato', 2019, 182)
+
 see=Serie('see', 2019, 1)
+the_morning_show=Serie('the morning show', 2019, 1)
 
-vingadores.dar_likes()
-vingadores.dar_likes()
+vingadores_g_i.dar_likes()
+vingadores_g_i.dar_likes()
+
+vingadores_u.dar_likes()
+vingadores_u.dar_likes()
 
 see.dar_likes()
 see.dar_likes()
 
-lista=[vingadores, see]
+the_morning_show.dar_likes()
+the_morning_show.dar_likes()
 
-for programa in lista:
+lista=[vingadores_g_i, vingadores_u, see, the_morning_show]
+minha_playlist = Playlist('fim de semana', lista)
+
+for programa in minha_playlist.listagem:
     print(programa)
+
+print('tamanho da playlist: {}'.format(len(minha_playlist.listagem)))
 
